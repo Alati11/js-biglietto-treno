2d437818c4f61ae5d8044dbf76a329d8690ccb53
Alati11 <peppealati@gmail.com>
2023-10-04 16:13:54 +0200

    add priceBase ticket

--- js/app.js
+++ js/app.js
@@ -3,11 +3,26 @@
 // - chiedere di inserire il numero di km da percorrere
 //     - dichiara una variabile
 //     - assegnare alla variabile il valore restituito da un prompt
+let numKm;
+numKm = prompt('Quanti km vuoi percorrere?');
+console.log(parseInt(numKm))
+
 // - inserire l'età del passeggero
 //     - dichiara una variabile
 //     - assegnare alla variabile il valore restituito da un prompt
-// - calcolare il prezzo il prezzo totale del viaggio
+let etaUser;
+etaUser = prompt('inserire età');
+console.log(parseInt(etaUser));
+
+
+// - calcolare il prezzo totale del viaggio
 //     - calcolare prezzo base (0.21 € al km)
+let price 
+price = (parseFloat(0.21));  
+let priceBase = numKm * price;
+// priceBase = priceBase.toFixed      
+console.log(parseFloat(priceBase));
+
 //     - va applicato uno sconto del 20% per i minorenni
 //     - va applicato uno sconto del 40% per gli over 65.
 // - stampare il prezzo finale    
